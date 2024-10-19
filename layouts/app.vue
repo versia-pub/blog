d4b3f39533cac12b19ac7cf7807f8378ecddc973
Jesse Wierzbinski <contact@cpluspatch.com>
2024-10-20 00:12:23 +0200

    feat: :sparkles: Add <main> landmark

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div :class="[$style.content, 'w-full h-full min-h-screen bg-gradient-to-tr from-zinc-900 to-zinc-800']">
-    <slot />
-  </div>
+    <main :class="[$style.content, 'w-full h-full min-h-screen bg-gradient-to-tr from-zinc-900 to-zinc-800']">
+        <slot />
+    </main>
 </template>
 
 <style lang="css" module>
